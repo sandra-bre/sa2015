54ddfeba177614ef358780950825b1c904586275
root <root@ubuntu>
2016-01-08 02:21:08 +0100

    Google map added

--- SA_WEB/sa_web_task4/searchstop.php
+++ SA_WEB/sa_web_task4/searchstop.php
@@ -13,93 +13,76 @@ and open the template in the editor.
     </head>
     <body class="result_map">
         <div class="top_bar">
-        <h1>Search Stop</h1>
-        
-        <a id="home" href="index.php">home</a>
+            <h1>Search Stop</h1>
+
+            <a id="home" href="index.php">home</a>
         </div>
-        
+
         <form id="searchstop" method="post">
             <div class="search_bar">
-            <div class="search_name">
-            <a>Name:</a><br>
-            <input type="text" name="stopname">
-            </div>
-            
-            <div class="search_point">
-            <a>Latitude:</a><br>
-            <input type="text" name="latvalue"><br>
-            <input type="radio" name="lattype" value="exact" checked="checked"><label for="latexact">exact</label>
-            <input type="radio" name="lattype" value="above"><label for="latabove">above</label>
-            <input type="radio" name="lattype" value="below"><label for="latbelow">below</label>
-            </div>
-            
-            <div class="search_point">
-            <a>Longitude:</a><br>
-            <input type="text" name="lonvalue"><br>
-            <input type="radio" name="lontype" value="exact" checked="checked"><label for="lonexact">exact</label>
-            <input type="radio" name="lontype" value="above"><label for="lonabove">above</label>
-            <input type="radio" name="lontype" value="below"><label for="lonbelow">below</label> 
-            </div>
+                <div class="search_name">
+                    <a>Name:</a><br>
+                    <input type="text" id="stopname">
+                </div>
+
+                <div class="search_point">
+                    <a>Latitude:</a><br>
+                    <input type="text" id="latvalue"><br>
+                    <input type="radio" name="lattype" value="exact" checked="checked"><label for="latexact">exact</label>
+                    <input type="radio" name="lattype" value="above"><label for="latabove">above</label>
+                    <input type="radio" name="lattype" value="below"><label for="latbelow">below</label>
+                </div>
+
+                <div class="search_point">
+                    <a>Longitude:</a><br>
+                    <input type="text" id="lonvalue"><br>
+                    <input type="radio" name="lontype" value="exact" checked="checked"><label for="lonexact">exact</label>
+                    <input type="radio" name="lontype" value="above"><label for="lonabove">above</label>
+                    <input type="radio" name="lontype" value="below"><label for="lonbelow">below</label> 
+                </div>
             </div>
         </form>
-        
+
         <div id="resultstops"></div>
         <div id="map"></div>
-        <script type="text/javascript">
-
-        var map;
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 47.067, lng: 15.441},
-                   zoom: 13
-            });
-        }
-
-        </script>
-        <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKdI4C7gQzK9jZi9Po9BZaBKwSe1FTcZE&callback=initMap">
-        </script>
     </body>
 </html>
 
 <script>
-    $(document).ready(function()
-    {
-        setInterval(function()
-        {
-            var name = document.getElementsByName('stopname')[0].value;
-            var lat = document.getElementsByName('latvalue')[0].value;
+ 
+        document.getElementById('stopname').addEventListener("keypress", getstops);
+        document.getElementById('latvalue').addEventListener("keypress", getstops);
+        document.getElementById('lonvalue').addEventListener("keypress", getstops);
+        document.getElementsByName('lattype')[0].addEventListener("click", getstops);
+        document.getElementsByName('lattype')[1].addEventListener("click", getstops);
+        document.getElementsByName('lattype')[2].addEventListener("click", getstops);
+        document.getElementsByName('lontype')[0].addEventListener("click", getstops);
+        document.getElementsByName('lontype')[1].addEventListener("click", getstops);
+        document.getElementsByName('lontype')[2].addEventListener("click", getstops);
+    function getstops()  {
+            var name = document.getElementById('stopname').value;
+            var lat = document.getElementById('latvalue').value;
             var lattype;
-            if(document.getElementsByName('lattype')[0].checked == true) {
+            if (document.getElementsByName('lattype')[0].checked == true) {
                 lattype = document.getElementsByName('lattype')[0].value;
-            }
-            else if (document.getElementsByName('lattype')[1].checked == true) {
+            } else if (document.getElementsByName('lattype')[1].checked == true) {
                 lattype = document.getElementsByName('lattype')[1].value;
-            }
-            else if (document.getElementsByName('lattype')[2].checked == true) {
+            } else if (document.getElementsByName('lattype')[2].checked == true) {
                 lattype = document.getElementsByName('lattype')[2].value;
             }
-            
-            var lon = document.getElementsByName('lonvalue')[0].value;
+
+            var lon = document.getElementById('lonvalue').value;
             var lontype;
-            if(document.getElementsByName('lontype')[0].checked == true) {
+            if (document.getElementsByName('lontype')[0].checked == true) {
                 lontype = document.getElementsByName('lontype')[0].value;
-            }
-            else if (document.getElementsByName('lontype')[1].checked == true) {
+            } else if (document.getElementsByName('lontype')[1].checked == true) {
                 lontype = document.getElementsByName('lontype')[1].value;
-            }
-            else if (document.getElementsByName('lontype')[2].checked == true) {
+            } else if (document.getElementsByName('lontype')[2].checked == true) {
                 lontype = document.getElementsByName('lontype')[2].value;
             }
-            
-            
-            var url = 'controller.php?function=1';
-            
-                url += '&stopname=' + name;
-            
-            
+
+
             $('#resultstops').load('controller.php?f=1&name=' + name + '&latvalue=' + lat + '&lattype=' + lattype
                     + '&lonvalue=' + lon + '&lontype=' + lontype);
-        }, 1000);
-    });
+        };
 </script>
